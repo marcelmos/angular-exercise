d2b01a04eea434487b5c8e343bfdab8ae0084375
marcelmos <kangurexplay@gmail.com>
2020-05-20 20:02:35 +0200

    Break

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course-practical-exercise';
+  newTask = '';
+  taskList = new Array<Task>();
+  setNewTask = false;
+
+  createNewTask(event){
+    this.newTask = event.target.value;
+  }
+
+  addToTaskList() {
+    this.setNewTask = true;
+  }
+
+  // deleteThis() {
+  // }
+
+  // completeThis() {
+  // }
+
+  constructor(){
+    if (this.newTask.length > 0 && this.setNewTask){
+      this.taskList.push(new Task(this.newTask, false));
+      this.setNewTask = false;
+    }
+  }
+}
+
+class Task{
+  constructor(public taskName: string, public status: boolean){
+  }
 }
